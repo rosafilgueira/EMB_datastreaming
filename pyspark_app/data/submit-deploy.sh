168cb6f5f85f20bae9b91aa4c507a2f497fd28e6
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2018-02-13 10:54:24 +0000

    rm intermediate files

--- pyspark_app/data/submit-deploy.sh
+++
@@ -1,2 +0,0 @@
-set -x 
-$SPARK_HOME/bin/spark-submit --class org.apache.spark.examples.SparkPi --master spark://192.5.87.53:7077 --deploy-mode cluster --supervise $SPARK_HOME/examples/jars/spark-examples_2.11-2.2.0.jar 100
--- pyspark_app/data/submit-master.sh
+++
@@ -1,4 +0,0 @@
-set -x 
-$SPARK_HOME/bin/spark-submit --class org.apache.spark.examples.SparkPi --master  
---master spark://129.114.108.142:7077 $SPARK_HOME/examples/jars/spark-examples_22
-.11-2.2.0.jar 100
