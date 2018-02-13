effded1d0bb8b1e087d627069b2d5f4f76374f23
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-02-13 10:26:36 +0000

    Delete list-topic.sh
    not needed

--- list-topic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# sudo docker ps -a 
-#./list-topic.sh dockersparkkafkastreaming_kafka_1 129.114.108.142:2181
-
-sudo docker exec -i $1 /opt/kafka/bin/kafka-topics.sh --list --zookeeper $2
