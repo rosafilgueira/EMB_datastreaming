49b7dee39b09a9a7d141fe07c63a28ef1920a4e6
rosafilgueira <rosa.filgueira@ed.ac.uk>
2018-02-14 12:18:59 +0000

    examples

--- examples_elastisearch_queries.sh
+++ examples_elastisearch_queries.sh
@@ -1,3 +1,29 @@
 
-curl -XGET 'http://localhost:9200/emb_test/emb/_search?q=%2Bsensor_id:emb3%20%2Bsec:844'
+#curl -XGET 'http://localhost:9200/emb_test/emb/_search?q=%2Bsensor_id:emb3%20%2Bsec:844'
+
+#curl -XGET 'http://localhost:9200/emb_test/emb/_search?pretty' -d'
+#{
+#	"query":{
+#		"range":{
+#		   "date":{
+#			"gt": "2016-12-01",
+#			"lt": "2017-01-10"
+#			}
+#		      }
+#	}
+#
+#}'
+
+curl -XGET 'http://localhost:9200/emb_test/emb/_search?q=sensor_id:emb3%20pretty' -d'
+{
+	"query":{
+		"range":{
+		   "date":{
+			"gte": "2017-01-01",
+			"lte": "2017-01-10"
+			}
+		      }
+	}
+
+}'
 
