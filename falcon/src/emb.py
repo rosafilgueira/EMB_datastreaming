082e6ba86867adba40613c3cb9e83ba45a767f78
rosafilgueira <rosa.filgueira@ed.ac.uk>
2018-02-16 13:44:14 +0000

    updates

---
+++ falcon/src/emb.py
@@ -0,0 +1,35 @@
+import falcon
+import json
+from falconjsonio.schema import request_schema
+import falconjsonio.middleware
+from kafka import KafkaProducer
+
+
+class BdrResource:
+    def on_post(self, req, resp):
+	print('On post req %s' % req)
+	msg = json.dumps(req.context['doc'], encoding='utf-8')
+	print('Brd falcoln %s' % msg)
+        producer = KafkaProducer(bootstrap_servers='kafka:9092')
+        producer.send('emb', msg)
+        producer.flush()
+        resp.status = falcon.HTTP_201
+        resp.body = msg
+
+class HealthCheckResource:
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps({"healthcheck":"ok"}, encoding='utf-8')
+
+app = falcon.API(
+    middleware=[
+        falconjsonio.middleware.RequireJSON(),
+        falconjsonio.middleware.JSONTranslator(),
+    ],
+)
+
+emb = BdrResource()
+healthcheck = HealthCheckResource()
+
+app.add_route('/emb', emb)
+app.add_route('/healthcheck', healthcheck)
