92c531143bd7fb3cbf1b4707c44570caa61aa898
rosafilgueira <rosa.filgueira.vicente@gmail.com>
2018-02-17 13:27:51 +0000

    sensor-simulator.py

--- sensordata/sensor-simulator.py
+++ sensordata/sensor-simulator.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+
+#python sensor_simulator.py -s emb3
+
+import argparse
 import requests
 import os 
 import sys
@@ -7,8 +11,17 @@ from time import sleep
 
 url = 'http://localhost:8000/emb'
 
-path="./EMB3_dataset_2017/"
-sensor_id = 'emb3'
+ 
+s_help = 'sensor to simulate, e.g. emb3' 
+description = 'Sensor id is required (-s is required).'
+parser = argparse.ArgumentParser(description = description)
+parser.add_argument("-s", help=s_help)
+args = parser.parse_args()
+
+sensor_id = args.s
+path= "./"+sensor_id.upper()+"_dataset_2017/"
+
+print ("path is %s" % path)
 
 for filename in os.listdir(path):
     sensor = str.encode(sensor_id + ',')
@@ -18,4 +31,4 @@ for filename in os.listdir(path):
             print("new line to send %s" % line_n)
             response = requests.post(url, data=line_n, headers={"Content-Type": "application/json"})
  	    print(response)
-            sleep(1)
+            sleep(3)
