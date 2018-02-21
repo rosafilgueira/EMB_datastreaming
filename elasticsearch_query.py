450fc46bfa5f943449b3662043bd27733e7323ce
Chameleon Cloud User <cc@rf.novalocal>
2018-02-21 16:09:40 +0000

    elasticsearch_query.py

--- elasticsearch_query.py
+++ elasticsearch_query.py
@@ -8,17 +8,12 @@ response = requests.get('http://localhost:9200/emb_test')
 print response
 
 #res = es.get(index="emb_test", doc_type="emb", id="AWG46Yeq17_Xh6R0pg12")
-#print res
-
 res = es.search(index="emb_test", doc_type="emb", body={"query": {"match_all":{}}})
-print res
-
-print("Got %d Hits:" % res['hits']['total'])
+print("Got %d elements:" % res['hits']['total'])
 for hit in res['hits']['hits']:
-	print("sensor_id %s - " % hit["_source"]["sensor_id"])
+	print("sensor_id:%(sensor_id)s, date-time: %(date)s %(time)s -- water_temp: %(water_temp)s, water_level: %(water_level)s, ph: %(ph)s, tdg: %(tdg)s, sec: %(sec)s -- quality check: %(qc)s" % hit["_source"])
 
 
-#u'sensor_id': u'"emb3', u'water_temp': 11.4, u'water_level': 28.26, u'tdg': 882, u'qc': u'normal', u'sec': 844, u'time': u'03:00:00', u'date': u'2017-01-03', u'ph': 7.21}
 
 
 
