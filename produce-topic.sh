52b8797f3a17138f250472cec8fe178c96ec0b2e
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-09-13 18:46:39 +0100

    Update produce-topic.sh

--- produce-topic.sh
+++ produce-topic.sh
@@ -2,5 +2,5 @@
 set -x 
 
 #./produce-topic.sh kafka kafka:9092 word_count pyspark_app/data/word_count.txt
-docker exec -i $1 /opt/kafka_2.12-1.0.0/bin/kafka-console-producer.sh --broker-list $2 --topic $3 < $4
+docker exec -i $1 /opt/kafka_2.11-2.0.0//bin/kafka-console-producer.sh --broker-list $2 --topic $3 < $4
 
