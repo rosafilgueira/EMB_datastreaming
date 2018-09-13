a44ddd7c7033873eb22c6e216890426f0a3f3621
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-09-13 18:53:53 +0100

    Update receive-topic.sh

--- receive-topic.sh
+++ receive-topic.sh
@@ -3,6 +3,6 @@ set -x
 
 # sudo docker ps -a 
 
-# ./receive-topic.sh kafka emb zookeeper:2181
-docker exec -i $1 /opt/kafka_2.11-2.0.0/bin/kafka-console-consumer.sh --topic=$2 --zookeeper=$3
+# ./receive-topic.sh
+sudo docker exec -i kafka /opt/kafka_2.11-2.0.0/bin/kafka-console-consumer.sh --topic=emb3 --bootstrap-server=kafka:9092
 
