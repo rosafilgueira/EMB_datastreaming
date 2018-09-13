f1b90ee2170573b2c92a4d6164c78110fe4fac7e
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2018-09-13 19:16:39 +0100

    Update receive-topic.sh

--- receive-topic.sh
+++ receive-topic.sh
@@ -3,6 +3,6 @@ set -x
 
 # sudo docker ps -a 
 
-# ./receive-topic.sh
-sudo docker exec -i kafka /opt/kafka_2.11-2.0.0/bin/kafka-console-consumer.sh --topic=emb3 --bootstrap-server=kafka:9092
+# ./receive-topic.sh kafka emb
+sudo docker exec -i $1 /opt/kafka_2.11-2.0.0/bin/kafka-console-consumer.sh --topic=$2 --bootstrap-server=kafka:9092
 
